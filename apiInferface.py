c6f0c47a9bd9b64613f61e904ea043c5729e29ff
puneetsharma2207 <puneet.sharma@gdn-commerce.com>
2023-12-15 23:58:03 +0530

    user_recommended_categories

--- apiInferface.py
+++ apiInferface.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
 import pysolr
+import requests
 
 app = Flask(__name__)
 
 SOLR_URL = "http://localhost:8983/solr/userCollection"  # Update with your Solr URL
+store_id = "10001"
+channel_id = "10001"
+client_id = "10001"
+request_id = "bbiibhu"
+username = "codiecon"
 
 
 def get_user_data(user_id):
@@ -60,7 +66,7 @@ def get_user_recommended_categories(user_id):
 
     # Construct the Solr query with the user_id
     solr_query = f"userId:{user_id}"
-    print(solr_query)
+    # print(solr_query)
 
     # Execute the Solr query and return the documents
     results = solr.search(solr_query, rows=10)  # You can adjust the number of rows as needed
@@ -69,20 +75,48 @@ def get_user_recommended_categories(user_id):
         categories = []
         if 'categories' in result:
             categories += result['categories']
-        print(categories)
+        # print(categories)
     return categories
+
+
+def get_user_recommended_categories_names(user_id):
+    categories = get_user_recommended_categories(user_id)
+    category_data_array = []
+
+    for category_code in categories:
+        category_data = fetch_category_data(category_code, store_id, channel_id, client_id, request_id, username)
+        # print(category_data)
+        if category_data:
+            category_data_array.append(category_data)
+    return category_data_array
+
+
+def fetch_category_data(category_code, store_id, channel_id, client_id, request_id, username):
+    base_url = "http://product-category-base.qa2-sg.cld/product-category-base/api/category/categoryCode/"
+    api_url = f"{base_url}{category_code}?storeId={store_id}&channelId={channel_id}&clientId={client_id}&requestId={request_id}&username={username}"
+
+    try:
+        response = requests.get(api_url)
+        # print(api_url)
+        response.raise_for_status()
+        category_data = response.json()
+        # print(category_data)
+        name = category_data.get("value", {}).get("nameEnglish")
+        return name
+        # Raise an exception for bad responses (4xx and 5xx status codes)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for category {category_code}: {e}")
+        return None
 
 
 def get_relevant_category_categories(user_id):
     categories = get_user_recommended_categories(user_id)
-    SOLR_URL = "http://brs-solr-1.qa2-sg.cld:8983/solr/userProfileCollection"  # Update with your Solr URL
+    SOLR_URL = "http://xsearch-solr-1.qa2-sg.cld:8983/solr/retailCollection"  # Update with your Solr URL
     solr = pysolr.Solr(SOLR_URL, timeout=10)
 
     # Construct the Solr query with the user_id
-    category_codes = ''
-    for category in categories:
-        category_codes += category
-    solr_query = f"salesCatalogCategoryIdsAll:{user_id}"
+    category_codes = " OR ".join(categories)
+    solr_query = f"salesCatalogCategoryIds:{category_codes}"
     print(solr_query)
 
     # Execute the Solr query and return the documents
@@ -122,7 +156,7 @@ def get_pre_order_products(user_id):
 @app.route('/getUserRecommendedCategories/<user_id>', methods=['GET'])
 def get_user_recommended__categories(user_id):
     try:
-        data = get_user_recommended_categories(user_id)
+        data = get_user_recommended_categories_names(user_id)
 
         return jsonify(data)
     except Exception as e:
@@ -132,7 +166,7 @@ def get_user_recommended__categories(user_id):
 @app.route('/getRelevantCategoryProducts/<user_id>', methods=['GET'])
 def get_relevant_categories_data(user_id):
     try:
-        data = get_user_recommended_categories(user_id)
+        data = get_relevant_category_categories(user_id)
 
         return jsonify(data)
     except Exception as e:
