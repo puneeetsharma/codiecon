63aa0d2568c22031dcb283676dd93a6b88cb8de8
puneetsharma2207 <puneet.sharma@gdn-commerce.com>
2023-12-18 16:34:44 +0530

    new api added

--- apiInferface.py
+++ apiInferface.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import apiLogic
+import requests
 
 app = Flask(__name__)
 
@@ -27,6 +28,16 @@ def get_user_savings(user_id):
 def get_pre_order_products(user_id):
     try:
         data = apiLogic.get_pre_order_products_data(user_id)
+
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/getPreOrderProductsByCategory/<user_id>', methods=['GET'])
+def get_pre_order_products_by_category(user_id):
+    try:
+        data = apiLogic.get_pre_order_products_data_for_category(user_id)
 
         return jsonify(data)
     except Exception as e:
@@ -63,5 +74,30 @@ def get_relevant_categories_products_data_sorted(user_id):
         return jsonify({'error': str(e)}), 500
 
 
+@app.route('/getAllBadgeDetails', methods=['GET'])
+def get_all_badge_details():
+    try:
+        return jsonify({"badges": apiLogic.get_all_badge_details()})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/getNearByStores', methods=['GET'])
+def search_pickup_points():
+    try:
+        lat = float(request.args.get('lat', 0.0))
+        lon = float(request.args.get('lon', 0.0))
+        return apiLogic.search_pickup_points_agp(lat, lon)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route("/")
+def test():
+    return "hi"
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="10.20.2.180", debug=True)
--- apiLogic.py
+++ apiLogic.py
@@ -1,5 +1,10 @@
 import pysolr
 import requests
+import json
+
+from flask import jsonify
+
+# import savingForecastModel
 
 SOLR_URL = "http://localhost:8983/solr/userCollection"  # Update with your Solr URL
 store_id = "10001"
@@ -10,20 +15,40 @@ username = "codiecon"
 
 
 def get_user_data(user_id):
+    global progress, current_batch
     solr = pysolr.Solr(SOLR_URL, timeout=10)
 
     # Construct the Solr query with the user_id
     solr_query = f"userId:{user_id}"
 
-    # Execute the Solr query and return the documents
-    results = solr.search(solr_query, rows=10)  # You can adjust the number of rows as needed
-    return results.docs
+    results = solr.search(solr_query, rows=10)
+    # Extract multivalued field from the Solr response
+    multivalued_field_values = []
+    for document in results.docs:
+        if 'achieved' in document:
+            multivalued_field_values.extend(document['achieved'])
+        progress = document.get('progress')
+        current_batch = document.get('currentBatch')
+
+    # Create a list of URLs by replacing the placeholder with multivalued field values
+    badge_urls = {
+        f"{value}": f"https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/{value}.png"
+        for i, value in enumerate(multivalued_field_values)
+    }
+    response_data = {
+        "user_id": user_id,
+        "achieved_badges": badge_urls,
+        "progress": progress,
+        "currentBatch": current_batch,
+    }
+    json_response = json.dumps(response_data)
+    return response_data
 
 
 def get_savings_data(user_id):
     SOLR_URL = "http://localhost:8983/solr/orderCollection"  # Update with your Solr URL
     solr = pysolr.Solr(SOLR_URL, timeout=10)
-    date_range = f"[NOW-12YEAR TO NOW]"
+    date_range = f"[NOW-1YEAR TO NOW]"
     # Construct the Solr query with the user_id
     solr_query = f"userId:{user_id} AND orderDate:{date_range}"
     # Execute the Solr query and return the documents
@@ -34,13 +59,26 @@ def get_savings_data(user_id):
     }
     results = solr.search(solr_query, **params)  # You can adjust the number of rows as needed
     total_discount = results.stats['stats_fields']['discountAvailed']['sum']
-
-    solr_query_last_month = f"userId:{user_id} AND orderDate:{date_range}"
+    date_range_month = f"[NOW-1MONTH TO NOW]"
+    solr_query_last_month = f"userId:{user_id} AND orderDate:{date_range_month}"
     # Execute the Solr query and return the documents
     results_month = solr.search(solr_query_last_month, **params)
-    total_discount_month = results.stats['stats_fields']['discountAvailed']['sum']
+    total_discount_month = results_month.stats['stats_fields']['discountAvailed']['sum']
+    params1 = {
+        'fl': 'savingPercentage',
+        'stats': 'true',
+        'stats.field': '{!sum=true}savingPercentage'
+    }
+    results_saving_percentage = solr.search(solr_query, **params1)
+    total_discount_saving_percentage = results_saving_percentage.stats['stats_fields']['savingPercentage']['sum']
 
-    return total_discount
+    api_response = {
+        "total_discount_saving_percentage": total_discount_saving_percentage,
+        "year_discount": total_discount,
+        "month_discount": total_discount_month,
+        # "saving_forecast": savingForecastModel.savings_forecast(total_discount)
+    }
+    return api_response
 
 
 def get_pre_order_products_data(user_id):
@@ -50,6 +88,24 @@ def get_pre_order_products_data(user_id):
     # Construct the Solr query with the user_id
     true = "true"
     solr_query = f"*:* AND isPreorder:{true}"
+    print(solr_query)
+
+    # Execute the Solr query and return the documents
+    results = solr.search(solr_query, rows=10)  # You can adjust the number of rows as needed
+    return results.docs
+
+
+def get_pre_order_products_data_for_category(user_id):
+    categories = get_user_recommended_categories(user_id)
+    SOLR_URL = "http://xsearch-solr-1.qa2-sg.cld:8983/solr/retailCollection"  # Update with your Solr URL
+    solr = pysolr.Solr(SOLR_URL, timeout=10)
+
+    # Construct the Solr query with the user_id
+    category_codes = " OR ".join(categories)
+    true = "true"
+    solr_query = f"salesCatalogCategoryIds:{category_codes} AND isPreorder:{true}"
+    # Construct the Solr query with the user_id
+    # solr_query = f"*:* AND isPreorder:{true}"
     print(solr_query)
 
     # Execute the Solr query and return the documents
@@ -133,5 +189,165 @@ def get_relevant_category_products_sort_by_price(user_id):
     sort_field = "salePrice"
     sort_order = "asc"
     # Execute the Solr query and return the documents
-    results = solr.search(solr_query, sort=f"{sort_field} {sort_order}", rows=10)  # You can adjust the number of rows as needed
+    results = solr.search(solr_query, sort=f"{sort_field} {sort_order}",
+                          rows=10)  # You can adjust the number of rows as needed
     return results.docs
+
+
+def get_all_badge_details():
+    all_badge_details = [
+        {
+            "name": "Perfect Cart",
+            "detail": "Bought more than 10+ products at once 10",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/perfect-cart.png"
+        },
+        {
+            "name": "Event Voyager",
+            "detail": "Bought tickets for more than 5 events",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/event-voyager.png"
+        },
+        {
+            "name": "Show Savvy",
+            "detail": "Bought tickets for more than 10 events",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/show-savvy.png"
+        },
+        {
+            "name": "Style Sculptor",
+            "detail": "Bought a fashion product which was worth  5k",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/style-sculptor.png"
+        },
+        {
+            "name": "Glamour Guru",
+            "detail": "Bought a fashion product which was worth  10k",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/glamour-guru.png"
+        },
+        {
+            "name": "Bundle Binge",
+            "detail": "Bought more than 10+ products at once 10",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/bundle-binge.png"
+        },
+        {
+            "name": "Mega Saver",
+            "detail": "Buying a product with more than  80%",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/mega-saver.png"
+        },
+        {
+            "name": "Ultra Saver",
+            "detail": "Buying a product with more than 90%",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/ultra-saver.png"
+        },
+        {
+            "name": "Rush Gizmo",
+            "detail": "Any tech product on 1st day",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/gizmo-rush.png"
+        },
+        {
+            "name": "Mobile Maestro",
+            "detail": "Buying mobile in 1st hour of launch",
+            "url": "https://raw.githubusercontent.com/akhilesh-k/temp-static-image-hosting/main/mobile-maestro.png"
+        },
+    ]
+    return all_badge_details
+
+
+def search_pickup_points_agp(lat, lon):
+    # Get latitude and longitude from request parameters
+
+    # Define the search query
+    search_url = 'http://aggregate-platform-query.qa2-sg.cld/api-native/business_partner_pickup_points/_search'
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Service-Id': 'pyeongyang-search'
+    }
+
+    query = {
+        "size": 10,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "exists": {
+                            "field": "displayName",
+                            "boost": 1.0
+                        }
+                    }
+                ],
+                "filter": [
+                    {
+                        "term": {
+                            "archived": {
+                                "value": False,
+                                "boost": 1.0
+                            }
+                        }
+                    },
+                    {
+                        "range": {
+                            "productCount": {
+                                "from": 0,
+                                "to": None,
+                                "include_lower": False,
+                                "include_upper": True,
+                                "boost": 1.0
+                            }
+                        }
+                    },
+                    {
+                        "geo_distance": {
+                            "geoPoint": [lon, lat],
+                            "distance": 20000.0,
+                            "distance_type": "plane",
+                            "validation_method": "STRICT",
+                            "ignore_unmapped": False,
+                            "boost": 1.0
+                        }
+                    }
+                ],
+                "adjust_pure_negative": True,
+                "boost": 1.0
+            }
+        },
+        "stored_fields": "_source",
+        "script_fields": {
+            "distance": {
+                "script": {
+                    "source": "doc['geoPoint'].arcDistance(params.lat, params.lon) * 0.001",
+                    "lang": "painless",
+                    "params": {
+                        "lon": lon,
+                        "lat": lat
+                    }
+                },
+                "ignore_failure": False
+            }
+        },
+        "sort": [
+            {
+                "productCount": {
+                    "order": "desc"
+                }
+            },
+            {
+                "_geo_distance": {
+                    "geoPoint": [{"lat": lat, "lon": lon}],
+                    "unit": "km",
+                    "distance_type": "plane",
+                    "order": "asc",
+                    "mode": "min",
+                    "validation_method": "STRICT",
+                    "ignore_unmapped": True
+                }
+            },
+            {
+                "displayName": {
+                    "order": "asc"
+                }
+            }
+        ]
+    }
+
+    # Make the search request
+    response = requests.post(search_url, headers=headers, json=query)
+    result = response.json()
+
+    return jsonify(result)
